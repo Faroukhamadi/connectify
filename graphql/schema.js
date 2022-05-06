b6791124421dce2062b6c8771eda54ecc1c42ace
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-05-06 09:58:02 +0100

    fix resolvers error

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,18 +1,17 @@
-const { gql } = require('apollo-server-micro');
+import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
     password: String
     first_name: String
     last_name: String
-    # friendship_friendship_friendTouser: friendship[]
-    # friendship_friendship_userTouser: friendship[]
-    # header_header_from_idTouser: header[]
-    # header_header_to_idTouser: header[]
+    # friendship_friendship_friendTouser: [ID]
+    # friendship_friendship_userTouser: [ID]
+    # header_header_from_idTouser: [ID]
+    # header_header_to_idTouser: [ID]
   }
-
   type Query {
     users: [User]!
   }
